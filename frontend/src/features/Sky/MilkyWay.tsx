e91429eecf24452d353ace64bbc317e16237540c
joey8angelo <joey8angelo@gmail.com>
2026-03-12 19:09:00 -0700

    error sphere on milkyway

--- frontend/src/features/Sky/MilkyWay.tsx
+++ frontend/src/features/Sky/MilkyWay.tsx
@@ -1,26 +1,29 @@
 import { useLoader } from "@react-three/fiber";
+import { useMemo } from "react";
 import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
 import * as THREE from "three";
-import { useMemo } from "react";
 
 export default function MilkyWay({ radius }: { radius: number }) {
-  const originalTexture = useLoader(
-    EXRLoader,
-    "/textures/milkyway_2020_4k.exr",
-  );
-  const texture = useMemo(() => {
-    const t = originalTexture.clone();
-    t.wrapS = THREE.RepeatWrapping;
-    t.repeat.x = -1;
+  const texture = useLoader(EXRLoader, "/textures/milkyway_2020_4k.exr");
+  const configuredTexture = useMemo(() => {
+    const nextTexture = texture.clone();
+    nextTexture.repeat.x = -1;
+    nextTexture.center.set(0.5, 0.5);
+    return nextTexture;
+  }, [texture]);
 
-    t.center.set(0.5, 0.5);
-    return t;
-  }, [originalTexture]);
+  if (!texture)
+    return (
+      <mesh position={[0, 0, 0]}>
+        <sphereGeometry args={[radius, 32, 32]} />
+        <meshStandardMaterial color="#ff0000" side={THREE.BackSide} />
+      </mesh>
+    );
 
   return (
     <mesh position={[0, 0, 0]}>
       <sphereGeometry args={[radius, 32, 32]} />
-      <meshStandardMaterial map={texture} side={THREE.BackSide} />
+      <meshStandardMaterial map={configuredTexture} side={THREE.BackSide} />
     </mesh>
   );
 }
