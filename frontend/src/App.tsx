8b64164e8f62368c67371498db4c6e4bd4a72c9f
joey8angelo <joey8angelo@gmail.com>
2026-04-18 18:10:26 -0700

    vercel.json and GA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,35 @@ import Map from "./pages/legacy/Map";
 import NeuralNet from "./pages/legacy/NeuralNet";
 import PlantSystem from "./pages/legacy/PlantSystem";
 import Particles from "./pages/Particles";
+import ReactGA from "react-ga4";
+import usePageTracking from "./hooks/usePageTracking";
+
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID, {
+  gaOptions: {
+    debug_mode: import.meta.env.DEV,
+  },
+});
+
+function AppRoutes() {
+  usePageTracking();
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/fourier" element={<Fourier />} />
+      <Route path="/ll1" element={<LL1 />} />
+      <Route path="/lr1" element={<LR1 />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/neural-net" element={<NeuralNet />} />
+      <Route path="/plant-system" element={<PlantSystem />} />
+      <Route path="/particles" element={<Particles />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/fourier" element={<Fourier />} />
-        <Route path="/ll1" element={<LL1 />} />
-        <Route path="/lr1" element={<LR1 />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/neural-net" element={<NeuralNet />} />
-        <Route path="/plant-system" element={<PlantSystem />} />
-        <Route path="/particles" element={<Particles />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
